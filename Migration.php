0e811ae5f271cc0388551f27cc7e1bfe687ab7f9
Marco Montagna <marcojoemontagna@gmail.com>
2015-03-24 03:07:03 +0000

    Uses cmd line arg

--- Migration.php
+++ Migration.php
@@ -223,6 +223,6 @@ class MigrationTable {
 $migration = new Test("test", true);
 $migration->change();*/
 
-Migration::run_all("./migrations", "test");
+Migration::run_all("./migrations", $argv[1]);
 
 
