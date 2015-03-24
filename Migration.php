aa9dc08760b33f7e83b6e7c8ac97169b3e4cf619
Alex <alexlily@gmail.com>
2015-03-24 04:27:14 +0000

    can add default values to columns

--- Migration.php
+++ Migration.php
@@ -174,8 +174,13 @@ class MigrationTable {
   }
 
 
-  function add_Column($name, $type) {
-    $STH = $this->DBH->prepare("ALTER TABLE `".$this->name."` ADD `$name` $type");
+  function add_Column($name, $type, $default = null) {
+  	if ($default == null){
+	    $STH = $this->DBH->prepare("ALTER TABLE `".$this->name."` ADD `$name` $type");  		
+  	} else{
+  		$STH = $this->DBH->prepare("ALTER TABLE `".$this->name."` ADD `$name` $type DEFAULT $default");
+  	}
+
     $STH->execute();
   }
 
@@ -183,21 +188,21 @@ class MigrationTable {
     $this->add_Column("timecreated", "timestamp default CURRENT_TIMESTAMP");
   }
   function timestamp($name) {
-    $this->add_Column($name, "timestamp");
+    $this->add_Column($name, "timestamp default 0");
   }
   function string($name, $size = 255) {
     $this->add_Column($name, "varchar($size)");
   }
-  function int($name) {
-    $this->add_Column($name, "int(11)");
+  function int($name, $default = null) {
+    $this->add_Column($name, "int(11)", $default);
   }
 
-  function reference($name) {
-    $this->int($name);
+  function reference($name, $default= null) {
+    $this->int($name, $default);
   }
 
-  function tinyint($name) {
-    $this->add_Column($name, "tinyint(4)");
+  function tinyint($name, $default = null) {
+    	$this->add_Column($name, "tinyint(4)", $default);
   }
 }
 
